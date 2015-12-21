f8537352c5393da94819e814a092d2781ff82847
Daniel Perez <daniel@claudetech.com>
2015-12-21 14:24:45 +0900

    Upgrade comeonin to 2.0.

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule SecurePassword.Mixfile do
   defp deps do
     [
       {:ecto, "~> 1.0"},
-      {:comeonin, "~> 1.6"},
+      {:comeonin, "~> 2.0"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev}
     ]
